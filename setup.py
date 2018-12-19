418b4ae35b69d59bc32cc09b26b3021bb92ea426
aep000 <alexparson1@gmail.com>
2018-12-19 13:58:21 -0500

    adding dist

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-from distutils.core import setup
-setup(name='pycache',
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(name='pycache',
       version='1.0',
+      description="Extensible General Purpose caching package",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/aep000/py_cache",
+      author="aep000",
+      keywords = "caching flask redis cache",
+      author_email="alexparson1@gmail.com",
       packages=['pycache'],
       )
